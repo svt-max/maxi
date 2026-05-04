2d22fd300d686203df64d801a781b6024f8a05e5
Stefan van Tulder <s.vantulder@maxcredible.com>
2026-05-04 14:06:30 +0200

    Fix email sender domain in send.js

--- api/send.js
+++ api/send.js
@@ -55,7 +55,7 @@ module.exports = async function handler(req, res) {
 
         const data = await resend.emails.send({
             // Update this line to use your verified domain
-            from: 'MaxCredible <noreply@maxcredible.com>', 
+            from: 'MaxCredible <noreply@maxi.maxcredible.com>', 
             to: [to], 
             subject: subject,
             html: html,
